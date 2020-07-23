b4ee3d002482bf0e731ae505c68bb999c7f1761c
LorenCheng <loren@agreeinfotech.com>
2020-07-23 15:45:21 +0800

    add starting message

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -2,6 +2,8 @@ import { natsWrapper } from './nats-wrapper';
 import { OrderCreatedListener } from './events/listeners/order-created-listener';
 
 const startApp = async () => {
+  console.log('Starting .......');
+
   if (!process.env.NATS_CLUSTER_ID) {
     throw new Error('NATS_CLUSTER_ID must be defined');
   }
