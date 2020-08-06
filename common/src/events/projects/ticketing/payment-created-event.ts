91b15b1425ca8232269802d6aad5dfd7532676fb
Loren Cheng <loren0223@gmail.com>
2020-08-06 11:16:09 +0800

    Remove common lib source code

--- common/src/events/projects/ticketing/payment-created-event.ts
+++
@@ -1,8 +0,0 @@
-import { Subjects } from '../../subjects';
-
-export interface PaymentCreatedEvent {
-  subject: Subjects.PaymentCreated;
-  data: {
-    orderId: string;
-  };
-}
--- common/src/events/types/order-status.ts
+++
@@ -1,6 +0,0 @@
-export enum OrderStatus {
-  Created = 'created',
-  Cancelled = 'cancelled',
-  AwaitPayment = 'awaitPayment',
-  Completed = 'completed',
-}
